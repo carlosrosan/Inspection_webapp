ec1d3c0c19414dad4606eb6866e36dc3e9cc032a
Carlos David Rodríguez Sánchez <carlosd2.rodriguez@gmail.com>
2025-09-04 22:47:02 -0300

    Update Mysql

--- Conuar/conuar_webapp/config/settings.py
+++ Conuar/conuar_webapp/config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'conuar_webapp'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('admin', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
