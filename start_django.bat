8434191ddf606cb96bec2aa564d988e4cb067870
Carlos David Rodríguez Sánchez <carlosd2.rodriguez@gmail.com>
2026-06-20 18:09:04 -0300

    Fix rutas django y nodered.bat

---
+++ start_django.bat
@@ -0,0 +1,11 @@
+@echo off
+cd C:\Users\usuario\Documents\GitHub\Inspection_webapp\
+call conuar_env\Scripts\activate
+cd C:\Users\usuario\Documents\GitHub\Inspection_webapp\
+python manage.py runserver
+pause
+
+
+
+
+
--- start_django_old.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd C:\Users\USER\Documents\GitHub\Inspection_webapp
-call conuar_env\Scripts\activate
-cd C:\Users\USER\Documents\GitHub\Inspection_webapp\Conuar\conuar_webapp
-python3 manage.py runserver
-pause
-
-
-
